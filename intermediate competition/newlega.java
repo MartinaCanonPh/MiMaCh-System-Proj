33efdb904be758c0a91c253bd067bccc9094f8fe
MartinaCanonPh <marti2f.mc@gmail.com>
2022-04-06 17:14:21 +0200

    Create newlega.java
    nuova lega raggiunta...lavoriamoci

---
+++ intermediate competition/newlega.java
@@ -0,0 +1,163 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Entity {
+
+    //variabili con visibilità di file
+    int unitId;
+    int unitType;
+    int player;
+    float mass;
+    int radius;
+    int x;
+    int y;
+    int vx;
+    int vy;
+    int extra;
+    int extra2;
+
+    //costruttore con parametri
+    public Entity(int unitId, int unitType, int player, float mass, int radius, int x, int y, int vx, int vy, int extra, int extra2) {
+        this.unitId = unitId;
+        this.unitType = unitType;
+        this.player = player;
+        this.mass = mass;
+        this.radius = radius;
+        this.x = x;
+        this.y = y;
+        this.vx = vx;
+        this.vy = vy;
+        this.extra = extra;
+        this.extra2 = extra2;
+    }
+
+    //costruttore senza parametri
+    public Entity()
+    {
+
+    }
+
+    double distance(int x2,int y2)
+    {
+        int resX=this.x-x2;
+
+        if(resX<0)
+            resX=resX*-1;
+        resX=resX*resX;
+        int resY=this.y-y2;
+
+        if(resY<0)
+            resY=resY*-1;
+        resY=resY*resY;
+        int result=resX+resY;
+
+        return java.lang.Math.sqrt(result);
+    }
+}
+
+class Player {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+
+        // game loop
+        while (true) {
+            int myScore = in.nextInt();
+            int enemyScore1 = in.nextInt();
+            int enemyScore2 = in.nextInt();
+            int myRage = in.nextInt();
+            int enemyRage1 = in.nextInt();
+            int enemyRage2 = in.nextInt();
+            int unitCount = in.nextInt();
+
+            //new structures
+            List<Entity> wrecks=new ArrayList<Entity>();
+            List<Entity> enemies=new ArrayList<Entity>();
+            Entity playerEntity=new Entity();
+
+            List<Entity> tanks=new ArrayList<Entity>();
+            Entity destroyerEntity=new Entity();
+
+            for (int i = 0; i < unitCount; i++) {
+                int unitId = in.nextInt();
+                int unitType = in.nextInt();
+                int player = in.nextInt();
+                float mass = in.nextFloat();
+                int radius = in.nextInt();
+                int x = in.nextInt();
+                int y = in.nextInt();
+                int vx = in.nextInt();
+                int vy = in.nextInt();
+                int extra = in.nextInt();
+                int extra2 = in.nextInt();
+
+                //adding a new Entity temp in the new structures
+                Entity temp=new Entity(unitId,unitType,player,mass,radius,x,y,vx,vy,extra,extra2);
+
+                if(unitType==4)                
+                    wrecks.add(temp);
+                
+                if(player==0)                
+                    playerEntity=temp;
+                
+                if(unitType==0 && player!=0)                
+                    enemies.add(temp);
+
+                if(unitType==1 && player!=-1)                
+                    destroyerEntity=temp;
+                
+                if(unitType==2)                
+                    tanks.add(temp);                 
+            }
+
+            //raggiungo la pozzanghera con distanza minima
+            double distance=12000;
+            Entity wreckMinDistance=new Entity();
+            for(Entity w:wrecks)
+            {   
+                if(distance > playerEntity.distance(w.x,w.y))
+                {
+                    distance = playerEntity.distance(w.x,w.y);
+                    wreckMinDistance = w;
+                }
+            }
+
+            //raggiungo il tank con distanza minima
+            double newDistance=12000;
+            Entity tankMinDistance=new Entity();
+            for(Entity t:tanks)
+            {   
+                if(newDistance > destroyerEntity.distance(t.x,t.y))
+                {
+                    distance = destroyerEntity.distance(t.x,t.y);
+                    tankMinDistance = t;
+                }
+            }
+
+             // Write an action using System.out.println()
+            // To debug: System.err.println("Debug messages...");
+
+            System.out.println(wreckMinDistance.x+" "+wreckMinDistance.y+" 200");
+            System.out.println("WAIT");
+            
+            System.out.println(tankMinDistance.x+" "+tankMinDistance.y+" 300");
+            System.out.println("WAIT");
+            System.out.println("WAIT");
+        }        
+    }
+
+    static boolean enemiesOnWreck(Entity wreck,List<Entity> enemies)
+    {
+        for(Entity e:enemies)
+        {        
+            if(wreck.distance(e.x,e.y)+e.radius<wreck.radius);  //??? da controllare e riscrivere
+                return true;
+        }        
+        return false;        
+    }
+}
